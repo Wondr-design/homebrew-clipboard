f2b974ad0022244f95f884ecab58463b35a685d5
Wondr <wondr.design@gmail.com>
2026-01-22 22:49:59 +0100

    Fix SHA256 from actual release asset

--- Formula/clipboardctl.rb
+++ Formula/clipboardctl.rb
@@ -6,7 +6,7 @@ class Clipboardctl < Formula
 
   on_macos do
     url "https://github.com/Wondr-design/smart-clipboard/releases/download/v0.1.0/clipboardctl-v0.1.0-darwin-universal.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "ab2cc9c27ce191d610509f0b32c66ab39d14513ac12c1e00ef524a3405c5e149"
   end
 
   depends_on :macos
